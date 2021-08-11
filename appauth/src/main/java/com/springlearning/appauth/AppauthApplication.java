98d9d9475082d9f503df415b894118f0ee1574f6
Zohaib Ahmed <zohaib.ahmed@techlogix.com>
2021-08-11 20:16:00 +0500

    first commit

---
+++ appauth/src/main/java/com/springlearning/appauth/AppauthApplication.java
@@ -0,0 +1,13 @@
+package com.springlearning.appauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppauthApplication.class, args);
+	}
+
+}
---
+++ appmvc/src/main/java/com/springlearning/appmvc/AppmvcApplication.java
@@ -0,0 +1,13 @@
+package com.springlearning.appmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmvcApplication.class, args);
+	}
+
+}
